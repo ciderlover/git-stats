35de31b4eec6e6a015e752c6a3bfb53305789c27
Bill <160423371+ciderlover@users.noreply.github.com>
2026-06-08 20:13:53 -0400

    Remove 'Prism_Launcher' from CONFIG label

--- api/rig.js
+++ api/rig.js
@@ -30,7 +30,7 @@ export default async function handler(req, res) {
             <text x="0" y="76" class="label">KERNEL:  <tspan class="value">Node.js / Vercel_Serverless</tspan></text>
             <text x="0" y="94" class="label">UPTIME:  <tspan class="value">100% (No-Crash Framework)</tspan></text>
             <text x="0" y="112" class="label">SHELL:   <tspan class="value">GIT Terminal</tspan></text>
-            <text x="0" y="130" class="label">CONFIG:  <tspan class="value">Sublime Text 4 / Prism_Launcher</tspan></text>
+            <text x="0" y="130" class="label">CONFIG:  <tspan class="value">Sublime Text 4</tspan></text>
         </g>
     </svg>
     `;
